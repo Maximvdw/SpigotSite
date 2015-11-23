01c32a8ba7de3e53e3ff2601c2806c6f0c15c81d
Maxim Van de Wynckel <maxim@mvdw-software.com>
2015-11-23 20:01:52 +0100

    Nop rate limit is not removed. But lowered tho, I think

--- src/main/java/be/maximvdw/spigotsite/SpigotSiteCore.java
+++ src/main/java/be/maximvdw/spigotsite/SpigotSiteCore.java
@@ -29,7 +29,7 @@ public class SpigotSiteCore implements SpigotSiteAPI {
 	private static Map<String, String> baseCookies = new HashMap<String, String>();
 	private static boolean firstStart = true;
 	private static String baseURL = "https://www.spigotmc.org/";
-	private static int rateLimitTimeout = 2000;
+	private static int rateLimitTimeout = 500;
 
 	public SpigotSiteCore() {
 		// Set managers
